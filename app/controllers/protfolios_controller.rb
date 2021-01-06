ccaf4d6371804108592240d338ef046121273682
masao <>
2021-01-07 06:28:41 +0900

    protfolioの詳細画面作成

--- app/controllers/protfolios_controller.rb
+++ app/controllers/protfolios_controller.rb
@@ -2,4 +2,8 @@ class ProtfoliosController < ApplicationController
   def index
     @protfolios = Protfolio.all.order(created_at: :desc)
   end
+
+  def show
+    @protfolio = Protfolio.find(params[:id])
+  end
 end
